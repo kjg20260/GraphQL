704b669e83bfcae060212526ddc5f0fd2a83824b
KJG <kjg20260@gmail.com>
2023-04-06 23:41:57 +0900

    graphql 학습

---
+++ tweetql/server.js
@@ -0,0 +1,57 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const tweets = [
+    {
+        id : "1",
+        text : "first"
+    },
+    {
+        id : "2",
+        text : "second"
+    },
+    {
+        id : "3",
+        text : "third"
+    }
+]
+
+const typeDefs = gql`
+    type User {
+        id : ID!
+        username : String!
+        firstName : String!
+        lastName : String
+    }
+
+    type Tweet {
+        id : ID!
+        text : String!
+        author : User
+    }
+
+    type Query {
+        allTweets : [Tweet!]!
+        tweet(id : ID!) : Tweet
+    }
+
+    type Mutation {
+        postTweet(text : String!, userId : ID!) :Tweet!
+        deleteTweet(id : ID!) : Boolean!
+    }
+`;
+
+const resolvers = {
+    Query : {
+        allTweets(){
+            return tweets
+        },
+        tweet(root, {id}){
+            return tweets.find(tweet => tweet.id === id);
+        }
+    }
+}
+const server = new ApolloServer({typeDefs,resolvers});
+
+server.listen().then(({url})=>{
+    console.log(`Running on ${url}`);
+})
